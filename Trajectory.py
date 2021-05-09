85b093d1c4c3aac24e71bd4c8a0fb38663a6052e
ecrireabaptiste <34769059+ecrireabaptiste@users.noreply.github.com>
2021-05-09 15:14:37 +0200

    Create Trajectory.py

---
+++ Trajectory.py
@@ -0,0 +1,44 @@
+class Leg:
+    def DefineLeg(self, dist_angle, curbature, printing, maxspeed):
+        self.__dist_angle = dist_angle
+        self.__curbature = curbature
+        self.__printing = printing
+        self.__maxspeed = maxspeed
+        return self
+      
+    def InputLegInfo(self):
+        self.__dist_angle = float(input('Enter distance or angle '))
+        self.__curbature = float(input('Enter Curbature '))
+        self.__printing = int(input('Enter 1 if print '))
+        self.__maxspeed = float(input('Enter Max Speed '))
+        
+    def PrintLegInfo(self):
+        print(self.__dist_angle, self.__curbature, self.__printing, self.__maxspeed)
+
+    def dist_angle(self):
+        return self.__dist_angle
+    def curbature(self):
+        return self.__curbature   
+    def printing(self):
+        return self.__printing
+    def maxspeed(self):
+        return self.__maxspeed
+
+def CreateTrajectory():
+    Trajectory = []
+    leg = Leg()
+    leg.DefineLeg(100, 0, 1, 10)
+    Trajectory.append(leg)
+    leg.DefineLeg(90, 999, 1, 10)
+    Trajectory.append(leg)
+    leg.DefineLeg(100, 0, 1, 10)
+    Trajectory.append(leg)
+    leg.DefineLeg(90, 999, 1, 10)
+    Trajectory.append(leg)
+    leg.DefineLeg(100, 0, 1, 10)
+    Trajectory.append(leg)
+    leg.DefineLeg(90, 999, 1, 10)
+    Trajectory.append(leg)
+    return Trajectory
+
+def PrintTrajectory(Trajectory):
